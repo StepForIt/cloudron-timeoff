7310f36fc373ea1859eae3621e0b54d907372787
Mathieu Monin <monin.mathieu@gmail.com>
2025-03-20 22:22:14 +0100

    Update

--- start.sh
+++ start.sh
@@ -4,10 +4,7 @@ set -eu
 export DATABASE_URL=${CLOUDRON_POSTGRESQL_URL}
 export PORT=3000
 
-cd /app
-
-echo "Mise à jour de la base de données..."
-npm run db-update
+cd /app/code
 
 echo "Démarrage de l'application..."
 exec gosu cloudron:cloudron npm start
